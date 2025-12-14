da32ce81e2a7df4c08a06be61205184fc16ed36e
Gahramanff <gahramanff@gmail.com>
2025-12-14 21:10:13 +0300

    Restapi mvc trainer getirme

--- FitnessCenter.WebApp/Controllers/TrainersApiController.cs
+++ FitnessCenter.WebApp/Controllers/TrainersApiController.cs
@@ -1,59 +1,31 @@
+using FitnessCenter.WebApp.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using FitnessCenter.WebApp.Data;
-using System.Linq;
 
-namespace FitnessCenter.WebApp.Controllers
+[ApiController]
+[Route("api/trainers")]
+public class TrainersApiController : ControllerBase
 {
-    [Route("api/trainers")]
-    [ApiController]
-    public class TrainersApiController : ControllerBase
+    private readonly AppDbContext _context;
+
+    public TrainersApiController(AppDbContext context)
     {
-        private readonly AppDbContext _context;
+        _context = context;
+    }
 
-        public TrainersApiController(AppDbContext context)
-        {
-            _context = context;
-        }
-
-        [HttpGet]
-        public IActionResult GetTrainers()
-        {
-            var trainers = _context.Trainers
-                .Include(t => t.Service)
-                .Select(t => new
-                {
-                    Id = t.Id,
-                    AdSoyad = t.Name,
-                    Uzmanlik = t.Speciality,
-                    VerdigiHizmet = t.Service.Name,
-                    Fiyat = t.Service.Price
-                })
-                .ToList();
-
-            return Ok(trainers);
-        }
-
-        [HttpGet("search")]
-        public IActionResult SearchTrainers(string serviceName)
-        {
-            if (string.IsNullOrEmpty(serviceName))
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var trainers = _context.Trainers.AsNoTracking()
+            .Select(t => new
             {
-                return BadRequest("Lütfen bir hizmet adý girin.");
-            }
+                id = t.Id,
+                name = t.Name,
+                speciality = t.Speciality,
+                imageUrl = t.ImageUrl
+            })
+            .ToList();
 
-            var trainers = _context.Trainers
-                .Include(t => t.Service)
-                .Where(t => t.Service.Name.Contains(serviceName)) 
-                .Select(t => new
-                {
-                    Id = t.Id,
-                    AdSoyad = t.Name,
-                    Branþ = t.Service.Name
-                })
-                .ToList();
-
-            return Ok(trainers);
-        }
+        return Ok(trainers);
     }
 }
---
+++ FitnessCenter.WebApp/Controllers/TrainersApiReq.cs
@@ -0,0 +1,22 @@
+﻿using FitnessCenter.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FitnessCenter.Controllers
+{
+    public class TrainersApiReq : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<Trainer> trainers = new();
+
+            HttpClient h = new HttpClient();
+            var resp = await h.GetAsync("https://localhost:7112/api/trainers");
+
+            var respStr = await resp.Content.ReadAsStringAsync();
+            trainers = JsonConvert.DeserializeObject<List<Trainer>>(respStr);
+
+            return View(trainers);
+        }
+    }
+}
